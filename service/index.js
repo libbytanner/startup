6f2784e9df626f5f5ed1c91e0b218f717dcaacbd
Libby Tanner <libbytanner05@gmail.com>
2025-03-10 15:07:36 -0600

    album submission working again

--- service/index.js
+++ service/index.js
@@ -53,7 +53,7 @@ apiRouter.delete('/auth/logout', async (req, res) => {
 });
 
 // // // Middleware to verify that the user is authorized to call an endpoint
-const verifyAuth = async (res, req, next) => {
+const verifyAuth = async (req, res, next) => {
     const user = await findUser('token', req.cookies[authCookieName]);
     if (user) {
         next();
